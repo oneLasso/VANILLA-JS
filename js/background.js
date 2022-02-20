971a482f1e743db5de85856cf97ebe05b2364551
oneLasso <96966685+oneLasso@users.noreply.github.com>
2022-02-21 00:54:50 +0900

    make_chromeabb_withJS

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
+bgImage.className = "bgimg";
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "ad5395d3f0ece8d827d8b30144ef2dc5";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `${data.name}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
